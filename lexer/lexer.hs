59adb35b5a6fa64ef2acc820f0f7f1fa16c7a0d5
Johann <47913920+2xrJohann@users.noreply.github.com>
2024-07-03 01:04:22 +1000

    Update lexer.hs
    add type signatures to lexer

--- lexer/lexer.hs
+++ lexer/lexer.hs
@@ -10,10 +10,13 @@ data T = T {
 } deriving Show
 
 new :: String -> T
+
 new "" = T { input = "", ch = Nothing}
+
 new input = T { input = input, ch = Just $ head input}
 
 next :: T -> T
+
 next lexer = case input lexer of
     "" -> T { input = "", ch = Nothing}
     _ -> let remainder = tail $ input lexer in
@@ -22,6 +25,7 @@ next lexer = case input lexer of
         T { input = remainder, ch = Just $ head remainder }
 
 lex :: T -> (T, Token.Token)
+
 lex lexer =
     case fmap processChar (ch lexer) of
     Nothing -> (lexer, Token.EOF)
@@ -30,7 +34,6 @@ lex lexer =
     processChar char =
         case char of
         x | isSpace x -> lex $ next lexer
-
         '=' -> peekEquals lexer Token.ASSIGN Token.EQUAL
         '!' -> peekEquals lexer Token.BANG Token.NOTEQUAL
         '+' -> (next lexer, Token.PLUS)
@@ -45,26 +48,27 @@ lex lexer =
         ')' -> (next lexer, Token.RPAREN)
         '{' -> (next lexer, Token.LBRACE)
         '}' -> (next lexer, Token.RBRACE)
-
         x | isDigit x -> let (int, newLexer) = readChars lexer isDigit
             in (newLexer, Token.INTEGER $ read int)
-
         x | isLetter x -> let (ident, newLexer) = readChars lexer isLetter
             in (newLexer, Keywords.lookupKeyword ident)
-
         x -> (next lexer, Token.ILLEGAL)
 
+    readChars :: T -> (Char -> Bool) -> (String, T)
+
     readChars lexer op =
-        let readIdentifier' lexer buffer =
+        let readChars' lexer buffer =
                 case ch lexer of
                 Nothing -> (reverse buffer, lexer)
                 Just character ->
                     if op character
                     then
-                        readIdentifier' (next lexer) (character : buffer)
+                        readChars' (next lexer) (character : buffer)
                     else
                         (reverse buffer, lexer)
-        in readIdentifier' lexer []
+        in readChars' lexer []
+
+    peekEquals :: T -> Token.Token -> Token.Token -> (T, Token.Token)
 
     peekEquals lexer default' peek =
         maybe (lexer, default') peekEquals' (ch nextLexer)
