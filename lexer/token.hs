84b3073bb8a495439a10faa2bb52f348035459cb
Johann <47913920+2xrJohann@users.noreply.github.com>
2024-09-24 01:04:41 +1000

    Update token.hs

--- lexer/token.hs
+++ lexer/token.hs
@@ -42,3 +42,33 @@ data Token =
     | ELSE
     | RETURN
     deriving (Eq, Show)
+
+showToken :: Token -> String
+showToken token = case token of
+    ILLEGAL        -> "ILLEGAL"
+    EOF            -> "EOF"
+    ASSIGN         -> "ASSIGN"
+    PLUS           -> "PLUS"
+    MINUS          -> "MINUS"
+    BANG           -> "BANG"
+    ASTERISK       -> "ASTERISK"
+    SLASH          -> "SLASH"
+    Lexer.Token.LT -> "LT"
+    Lexer.Token.GT -> "GT"
+    EQUAL          -> "EQUAL"
+    NOTEQUAL       -> "NOTEQUAL"
+    COMMA          -> "COMMA"
+    SEMICOLON      -> "SEMICOLON"
+    FUNCTION       -> "FUNCTION"
+    LET            -> "LET"
+    LPAREN         -> "LPAREN"
+    RPAREN         -> "RPAREN"
+    LBRACE         -> "LBRACE"
+    RBRACE         -> "RBRACE"
+    INTEGER _      -> "INTEGER"
+    IDENT _        -> "IDENT"
+    TRUE           -> "TRUE"
+    FALSE          -> "FALSE"
+    IF             -> "IF"
+    ELSE           -> "ELSE"
+    RETURN         -> "RETURN"
